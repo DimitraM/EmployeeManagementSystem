7ea3a6320d7f85f3ac75fab2afedf338ee14224c
DimitraMavridou <dmavrido@netcompany.com>
2025-12-10 16:33:54 +0200

    Get All Employees

--- ems-backend/src/main/java/net/dimitra/ems_backend/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/net/dimitra/ems_backend/ems/service/EmployeeService.java
@@ -2,10 +2,14 @@ package net.dimitra.ems_backend.ems.service;
 
 import net.dimitra.ems_backend.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService
 {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 
 }
