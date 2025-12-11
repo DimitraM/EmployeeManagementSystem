b8073ef95600f051258ceb290015697ab012d9c5
DimitraMavridou <dmavrido@netcompany.com>
2025-12-11 17:51:18 +0200

    React Application as Frontent

---
+++ ems-front/ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FooterComponent() {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved to 2025 by DimitraM</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ ems-front/ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className="navbar navbar-light navbarExtra">
+                <a className="navbar-brand" href='https://github.com/DimitraM/EmployeeManagementSystem'>Employee Management System</a>
+            </nav>
+        </header>
+
+
+    </div>
+  )
+}
+
+export default HeaderComponent
